243d1a9c59475997b433893d93a4d0e3093b077c
noaweissfradin <noa.weiss@yahoo.fr>
2022-09-25 16:34:28 +0200

    dft without the recursive function

--- src/dft.py
+++ src/dft.py
@@ -5,6 +5,8 @@ from tree import T
 from collections import deque
 
 def in_order_L(t: T | None, L: list[int]):
+    """In-order traversal of a tree
+    Recursive function to understand the concept"""
     if(t!=None):
         in_order_L(t.left,L)
         L.append(t.val)
@@ -19,7 +21,10 @@ def in_order(t: T | None) -> Iterable[int]:
     [1, 2, 3, 4, 5]
     """
     L = []
+    #recursive function
     #in_order_L(t, L)
+
+    #stack function
     S = []
     if(t!=None):
         S.append(t)
